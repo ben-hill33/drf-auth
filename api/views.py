e9bd5824260999310dc8ac4a5be2b88a8d292d12
BEN <benjamin.hill28@gmail.com>
2021-03-25 11:14:21 -0700

    added view and urlpatterns to app and project, tested in browser

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+  ListCreateAPIView,
+  RetrieveUpdateDestroyAPIView
+)
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskList(ListCreateAPIView):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+
+class TaskDetail(RetrieveUpdateDestroyAPIView):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
