9beba0c2a2b627a090a235c94e6bbddcc7a98b3d
BEN <benjamin.hill28@gmail.com>
2021-03-25 17:08:41 -0700

    added jwt routes to project urls, added static root and default permission and authentication to settings

--- api/views.py
+++ api/views.py
@@ -1,15 +1,16 @@
-from rest_framework.generics import (
-  ListCreateAPIView,
-  RetrieveUpdateDestroyAPIView
-)
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+from .permissions import IsOwnerOrReadOnly
 from .models import Task
 from .serializers import TaskSerializer
 
 class TaskList(ListCreateAPIView):
   queryset = Task.objects.all()
   serializer_class = TaskSerializer
+  permission_classes = (IsAuthenticatedOrReadOnly,)
 
 class TaskDetail(RetrieveUpdateDestroyAPIView):
   queryset = Task.objects.all()
   serializer_class = TaskSerializer
+  permission_classes = (IsOwnerOrReadOnly,)
--- todo_drf_auth/settings.py
+++ todo_drf_auth/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'todo_drf_auth.urls'
@@ -123,3 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
